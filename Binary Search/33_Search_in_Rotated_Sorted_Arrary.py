6daf1ac775d344ce220056e07545dd5336eca7f5
Sean Currlin <sean.currlin@gmail.com>
2025-10-24 11:05:56 -0400

    add Binary Search problems

---
+++ Binary Search/33_Search_in_Rotated_Sorted_Arrary.py
@@ -0,0 +1,20 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        n, t = nums, target
+
+        while l <= r:
+            m = (l + r)//2
+            if t == n[m]:
+                return m
+            if n[l] <= n[m]:
+                if t > n[m] or t < n[l]:
+                    l = m + 1
+                else:
+                    r = m - 1
+            else:
+                if t < n[m] or t > n[r]:
+                    r = m - 1
+                else:
+                    l = m + 1
+        return -1
