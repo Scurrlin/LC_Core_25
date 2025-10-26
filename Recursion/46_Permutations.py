18cdcf61fea78dab0ad7eb3cfea5f5ef60a2bf12
Sean Currlin <sean.currlin@gmail.com>
2025-10-26 11:45:52 -0400

    update solution for 46

--- Recursion/46_Permutations.py
+++ Recursion/46_Permutations.py
@@ -1,11 +1,11 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         def backtrack(nums, path): 
-            if not nums: 
-                res.append(path) 
-                return 
+            if not nums: return res.append(path)
             for i in range(len(nums)): 
-                backtrack(nums[:i] + nums[i+1:], path + [nums[i]]) 
+                backtrack(
+                    nums[:i] + nums[i + 1:],
+                    path + [nums[i]]) 
         res = [] 
         backtrack(nums, []) 
         return res
