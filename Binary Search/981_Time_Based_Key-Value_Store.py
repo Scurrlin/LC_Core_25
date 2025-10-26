ca22c9f32f039f195356746055acbecf24ad404e
Sean Currlin <sean.currlin@gmail.com>
2025-10-26 01:16:03 -0400

    update solutions for Binary Search folder

--- Binary Search/981_Time_Based_Key-Value_Store.py
+++ Binary Search/981_Time_Based_Key-Value_Store.py
@@ -1,22 +1,20 @@
 class TimeMap:
-
     def __init__(self):
-        self.dic = {}
+        self.keys = {}
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        if key not in self.dic:
-            self.dic[key] = []
-        self.dic[key].append([value , timestamp])
+        if key not in self.keys:
+            self.keys[key] = []
+        self.keys[key].append([value, timestamp])
 
     def get(self, key: str, timestamp: int) -> str:
-        res = ""
-        values = self.dic.get(key , [])
-        l , r = 0 , len(values) - 1
-        while l <= r :
-            mid = (l + r) >> 1
-            if values[mid][1] <= timestamp:
-                l = mid + 1
-                res = values[mid][0]
+        res, values = "", self.keys.get(key, [])
+        l, r = 0, len(values) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if values[m][1] <= timestamp:
+                res = values[m][0]
+                l = m + 1
             else:
-                r = mid - 1
+                r = m - 1
         return res
