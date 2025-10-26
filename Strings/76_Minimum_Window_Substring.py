c2eddf85abc9e4602733723e30035464c9e99aa6
Sean Currlin <sean.currlin@gmail.com>
2025-10-26 11:45:17 -0400

    update solution for 76

--- Strings/76_Minimum_Window_Substring.py
+++ Strings/76_Minimum_Window_Substring.py
@@ -1,27 +1,23 @@
+from collections import Counter
+
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if len(s) < len(t): return ""
-        countT, window = {}, {}
-        for c in t:
-            countT[c] = 1 + countT.get(c, 0)
-
-        have, need = 0, len(countT)
-        res, resLen = [-1, -1], float("infinity")
-        l = 0
-
-        for r in range(len(s)):
-            c = s[r]
-            window[c] = 1 + window.get(c, 0)
-            if c in countT and window[c] == countT[c]:
-                have += 1
-            while have == need:
-                if (r - l + 1) < resLen:
-                    res = [l, r]
-                    resLen = r - l + 1
-                window[s[l]] -= 1
-                if s[l] in countT and window[s[l]] < countT[s[l]]:
-                    have -= 1
+        if not t or not s: return ""
+        need, missing = Counter(t), len(t)
+        l = start = end = 0
+        
+        for r, char in enumerate(s, 1):
+            if need[char] > 0:
+                missing -= 1
+            need[char] -= 1
+            if missing == 0:
+                while l < r and need[s[l]] < 0:
+                    need[s[l]] += 1
+                    l += 1
+                if end == 0 or r - l <= end - start:
+                    start, end = l, r
+                need[s[l]] += 1
+                missing += 1
                 l += 1
 
-        l, r = res
-        return s[l : r + 1] if resLen != float("infinity") else ""
+        return s[start:end]
