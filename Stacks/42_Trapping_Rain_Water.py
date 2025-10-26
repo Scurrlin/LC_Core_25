a24673e2743f28f54ded44a85b42d36e4550a4ec
Sean Currlin <sean.currlin@gmail.com>
2025-10-26 11:33:52 -0400

    update solution for 42

--- Stacks/42_Trapping_Rain_Water.py
+++ Stacks/42_Trapping_Rain_Water.py
@@ -1,19 +1,17 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
         h = height
-        if not h:
-            return 0
-
+        if not h: return 0
         l, r = 0, len(h) - 1
-        leftMax, rightMax = h[l], h[r]
-        res = 0
+        leftMax, rightMax, volume = h[l], h[r], 0
+
         while l < r:
             if leftMax < rightMax:
                 l += 1
                 leftMax = max(leftMax, h[l])
-                res += leftMax - h[l]
+                volume += leftMax - h[l]
             else:
                 r -= 1
                 rightMax = max(rightMax, h[r])
-                res += rightMax - h[r]
-        return res
+                volume += rightMax - h[r]
+        return volume
